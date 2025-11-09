5449d5103119e0761ab1b23d5357a44050ecd4d1
ilbd_culuv <xuanson522004@gmail.com>
2025-11-09 17:07:25 +0700

    add src and data folder

---
+++ src/test_data.py
@@ -0,0 +1,331 @@
+happy_case = [
+    {
+        "image": "data/happy_case/h1.jpg",
+        "ques": "Yếu tố nào có Độ lệch chuẩn (SD) là 3.92?",
+        "ans": "sIL-6"
+    },
+    {
+        "image": "data/happy_case/h1.jpg",
+        "ques": "Giá trị trung bình (Mean) của yếu tố sVEGF là bao nhiêu?",
+        "ans": "356.4"
+    },
+    {
+        "image": "data/happy_case/h2.jpg",
+        "ques": "dbSNP id của SNP 3 là gì?",
+        "ans": "rs1810132"
+    },
+    {
+        "image": "data/happy_case/h2.jpg",
+        "ques": "Sự thay đổi base (Base change) cho SNP nằm ở 'exon 17' là gì?",
+        "ans": "A>G"
+    },
+    {
+        "image": "data/happy_case/h3.jpg",
+        "ques": "Tần số (Frequency) của Haplotype (4) trong nhóm cases là bao nhiêu?",
+        "ans": "0.13"
+    },
+    {
+        "image": "data/happy_case/h3.jpg",
+        "ques": "Allele của Haplotype (2) là gì?",
+        "ans": "ctcIP"
+    },
+    {
+        "image": "data/happy_case/h4.jpg",
+        "ques": "Có bao nhiêu bệnh nhân đang nhận thuốc DMARDs (MTX 15 mg/wk)?",
+        "ans": "2"
+    },
+    {
+        "image": "data/happy_case/h4.jpg",
+        "ques": "Thời gian bệnh trung bình (Mean duration of disease) của bệnh nhân là bao lâu?",
+        "ans": "19 (1–48) months"
+    },
+    {
+        "image": "data/happy_case/h5.jpg",
+        "ques": "Số lỗi kê đơn (ERRORS OF PRESCRIPTION WRITING) của bác sĩ JUNIOR là bao nhiêu?",
+        "ans": "68 (54%)"
+    },
+    {
+        "image": "data/happy_case/h5.jpg",
+        "ques": "Tỷ lệ lỗi liên quan đến thuốc (DRUG RELATED ERRORS) của bác sĩ SENIOR là bao nhiêu?",
+        "ans": "0%"
+    },
+    {
+        "image": "data/happy_case/h6.jpg",
+        "ques": "Tuổi trung bình (Age) của nhóm 'With limited SSc(n=19)' là bao nhiêu?",
+        "ans": "46.5 ± 2.9"
+    },
+    {
+        "image": "data/happy_case/h6.jpg",
+        "ques": "Tỷ lệ phần trăm bệnh nhân nữ (% female) trong nhóm 'With diffuse SSc' là bao nhiêu?",
+        "ans": "95.7"
+    },
+    {
+        "image": "data/happy_case/h7.jpg",
+        "ques": "Giá trị trung vị (Median) của Rheumatoid factor (U/l) là bao nhiêu?",
+        "ans": "27.0"
+    },
+    {
+        "image": "data/happy_case/h7.jpg",
+        "ques": "Độ lệch chuẩn (Standard deviation) của Leucocytes (g/l) là bao nhiêu?",
+        "ans": "2.2"
+    },
+    {
+        "image": "data/happy_case/h8.jpg",
+        "ques": "Tỷ lệ phần trăm Macrophages ở chủng chuột JNK1-/-hTNFtg là bao nhiêu?",
+        "ans": "50.5 ± 5.3"
+    },
+    {
+        "image": "data/happy_case/h8.jpg",
+        "ques": "Tỷ lệ phần trăm Granulocytes ở chủng chuột hTNFtg là bao nhiêu?",
+        "ans": "17.7 ± 1.5"
+    },
+    {
+        "image": "data/happy_case/h9.jpg",
+        "ques": "Giá trị OR (Odds Ratio) của 'Age (per year older)' là bao nhiêu?",
+        "ans": "1.124"
+    },
+    {
+        "image": "data/happy_case/h9.jpg",
+        "ques": "Khoảng tin cậy 95% (95% CI) của 'SOFA score at surgery (per point)' là bao nhiêu?",
+        "ans": "1.076–1.695"
+    },
+    {
+        "image": "data/happy_case/h10.jpg",
+        "ques": "Tổng số bệnh nhân bị nhiễm trùng đường tiết niệu (Urinary tract)  là bao nhiêu?",
+        "ans": "6"
+    },
+    {
+        "image": "data/happy_case/h10.jpg",
+        "ques": "Tổng số bệnh nhân bị bệnh về phổi (LungLung)  là bao nhiêu?",
+        "ans": "44"
+    }
+]
+
+
+dense_case = [
+    {
+        "image": "data/dense_case/d1.jpg",
+        "ques": "Trong tập dữ liệu 'Paternal age dataset', có bao nhiêu Cases người da trắng (White)?",
+        "ans": "171"
+    },
+    {
+        "image": "data/dense_case/d1.jpg",
+        "ques": "Tập dữ liệu nào có 'Birth order, Maternal age' là Main exposure?",
+        "ans": "Maternal age dataset"
+    },
+    {
+        "image": "data/dense_case/d2.jpg",
+        "ques": "Bệnh lý (Human disease) của R. conorii (Morocco) là gì?",
+        "ans": "Mediterranean spotted fever"
+    },
+    {
+        "image": "data/dense_case/d2.jpg",
+        "ques": "Bao nhiêu lần Portugall62 được Passage (Passage history)?",
+        "ans": "4"
+    },
+    {
+        "image": "data/dense_case/d3.jpg",
+        "ques": "Chiến lược tìm kiếm nào (Top 3) có độ chính xác (Accuracy) cao nhất (65.3%)?",
+        "ans": "risk.mp. OR exp methodology OR exp mortality"
+    },
+    {
+        "image": "data/dense_case/d3.jpg",
+        "ques": "Trong nhóm 'Top 3-performing combination of terms with best Specificity', chiến lược có Sensitivity 53.5% có Precision là bao nhiêu?",
+        "ans": "6.9 (5.7 to 8.1)"
+    },
+    {
+        "image": "data/dense_case/d4.jpg",
+        "ques": "Trong nhóm 'NAT2 fast acetylator' và 'SULT1A1*1/*1 genotype', số lượng Cases hút thuốc '1-10 pack-years' là bao nhiêu?",
+        "ans": "22"
+    },
+    {
+        "image": "data/dense_case/d4.jpg",
+        "ques": "Trong nhóm 'NAT2 slow acetylator' và 'SULT1A1*2 allele carrier', OR (95% CI) của người hút thuốc '11+ pack-years' là bao nhiêu?",
+        "ans": "2.07 (0.91–4.72)"
+    },
+    {
+        "image": "data/dense_case/d5.jpg",
+        "ques": "Trong nhóm 'Micrometastases (CK-19 RGE) $\\ge$ 0.77', số lượng bệnh nhân có Vascularisation 'High' là bao nhiêu?",
+        "ans": "23 (70%)"
+    },
+    {
+        "image": "data/dense_case/d5.jpg",
+        "ques": "Tỷ lệ bệnh nhân có Grade I và Vascularisation 'Low' là bao nhiêu?",
+        "ans": "10 (37%)"
+    },
+    {
+        "image": "data/dense_case/d6.jpg",
+        "ques": "Vào lúc 23.00, kích thước đồng tử (Pupil size) của bệnh nhân là bao nhiêu?",
+        "ans": "3 mm"
+    },
+    {
+        "image": "data/dense_case/d6.jpg",
+        "ques": "Liều Atropine Infusion đầu tiên được ghi nhận là bao nhiêu?",
+        "ans": "2 mg/h"
+    },
+    {
+        "image": "data/dense_case/d7.jpg",
+        "ques": "Thời gian nằm ICU (ICU stay (days)) của Bệnh nhân 10 là bao lâu?",
+        "ans": "204"
+    },
+    {
+        "image": "data/dense_case/d7.jpg",
+        "ques": "Bệnh nhân nào có kết quả Clinical outcome là 'Complete recovery of legs, distal spasticity of arms'?",
+        "ans": "5"
+    },
+    {
+        "image": "data/dense_case/d8.jpg",
+        "ques": "Trong mô hình 'Separate GP cooperative', giá trị SD của 'Age' là bao nhiêu?",
+        "ans": "0.366"
+    },
+    {
+        "image": "data/dense_case/d8.jpg",
+        "ques": "Trong mô hình 'Integrated GP cooperative', giá trị Significance của 'Experienced workload' là bao nhiêu?",
+        "ans": "<0.001"
+    },
+    {
+        "image": "data/dense_case/d9.jpg",
+        "ques": "Trong nhóm LPS, giá trị pHa tại 'Time point (hours) 4' là bao nhiêu?",
+        "ans": "7.55 ± 0.02*"
+    },
+    {
+        "image": "data/dense_case/d9.jpg",
+        "ques": "Tại 'Time point (hours) 2', giá trị PaCO₂ (mmHg) của nhóm OA là bao nhiêu?",
+        "ans": "42.5 ± 3.7"
+    },
+    {
+        "image": "data/dense_case/d10.jpg",
+        "ques": "Có bao nhiêu bệnh nhân trong nhóm 'Midazolam-positive group' được chẩn đoán là 'Severe sepsis'?",
+        "ans": "0"
+    },
+    {
+        "image": "data/dense_case/d10.jpg",
+        "ques": "Tỷ lệ tử vong do mọi nguyên nhân (All-cause mortality) của nhóm 'Midazolam-negative group' là bao nhiêu?",
+        "ans": "8 (62%)"
+    }
+]
+
+
+
+structural_case = [
+    {
+        "image": "data/structural_case/s1.jpg",
+        "ques": "Trong 'Full birthweight dataset' và nhóm African-American, có bao nhiêu Controls trong phần Minimally adjusted OR?",
+        "ans": "n=89"
+    },
+    {
+        "image": "data/structural_case/s1.jpg",
+        "ques": "Giá trị OR (95% CI) đã được điều chỉnh đầy đủ (Fully adjusted OR) của Upper tertile trong Restricted dataset cho nhóm African-American là bao nhiêu?",
+        "ans": "0.3(0.1–1.2)"
+    },
+    {
+        "image": "data/structural_case/s1.jpg",
+        "ques": "Giá trị Median của nhóm White trong 'Restricted dataset' là bao nhiêu?",
+        "ans": "3232"
+    },
+    {
+        "image": "data/structural_case/s2.jpg",
+        "ques": "Trong 'Full CBCS' và nhóm African-American, có bao nhiêu Cases là '1st born'?",
+        "ans": "106"
+    },
+    {
+        "image": "data/structural_case/s2.jpg",
+        "ques": "Trong nhóm White, Born $\ge$1948, OR (95% CI) đã được điều chỉnh đầy đủ (Minimally adjusted OR) của '2nd–4th born' là bao nhiêu?",
+        "ans": "1.2 (0.8–1.9)"
+    },
+    {
+        "image": "data/structural_case/s2.jpg",
+        "ques": "Giá trị Range của nhóm African-American, Born $\\ge$1948 là bao nhiêu?",
+        "ans": "1–14"
+    },
+    {
+        "image": "data/structural_case/s3.jpg",
+        "ques": "Trong nhóm 'All cases', Hazard ratio (95% CI) của biến np trong Model 9 là bao nhiêu?",
+        "ans": "0.998 (0.992–1.005)"
+    },
+    {
+        "image": "data/structural_case/s3.jpg",
+        "ques": "Trong nhóm 'Node-positive patients', Hazard ratio của 'np/nx (%)' trong Model 5 là bao nhiêu?",
+        "ans": "1.017 (1.016–1.018)"
+    },
+    {
+        "image": "data/structural_case/s3.jpg",
+        "ques": "Trong nhóm 'Node-negative patients', giá trị L (Hazard ratio) của Model 6 là bao nhiêu?",
+        "ans": "1.150 (1.058–1.249)"
+    },
+    {
+        "image": "data/structural_case/s4.jpg",
+        "ques": "Trong nhóm 'SULT1A1*1/*1 genotype', OR (95% CI) của 'Current active' smoker là bao nhiêu?",
+        "ans": "1.44 (0.76–2.71)"
+    },
+    {
+        "image": "data/structural_case/s4.jpg",
+        "ques": "Số lượng Cases (n=221) trong nhóm 'SULT1A1*2 allele carrier' hút '10-19' cigarettes/day là bao nhiêu?",
+        "ans": "60"
+    },
+    {
+        "image": "data/structural_case/s4.jpg",
+        "ques": "Đối với 'Duration of passive exposure in adulthood', OR (95% CI) của '14+ years' trong nhóm 'SULT1A1*1/*1 genotype' là bao nhiêu?",
+        "ans": "1.66 (0.79–3.48)"
+    },
+    {
+        "image": "data/structural_case/s5.jpg",
+        "ques": "Giá trị PVR (dyne·s/cm⁵ per m²) của nhóm LPS tại 'Time point (hours) 1' là bao nhiêu?",
+        "ans": "284 ± 20**"
+    },
+    {
+        "image": "data/structural_case/s5.jpg",
+        "ques": "Giá trị HR (beats/min) của nhóm Sham tại 'Time point (hours) 3' là bao nhiêu?",
+        "ans": "91 ± 5"
+    },
+    {
+        "image": "data/structural_case/s5.jpg",
+        "ques": "Tại 'Time point (hours) 6', CI (l/min per m²) của nhóm LPS là bao nhiêu?",
+        "ans": "6.8 ± 0.2**"
+    },
+    {
+        "image": "data/structural_case/s6.jpg",
+        "ques": "Trong nhóm '>60 and <65 years', tỷ lệ bệnh nhân có toxicity độ 3 (Grade 3) do Leukopenia là bao nhiêu?",
+        "ans": "7"
+    },
+    {
+        "image": "data/structural_case/s6.jpg",
+        "ques": "Độc tính nào có tỷ lệ bệnh nhân (phần trăm) có độc tính là 8 ở mức độ 3 trong nhóm ≥ 65 years",
+        "ans": "Nausea"
+    },
+    {
+        "image": "data/structural_case/s6.jpg",
+        "ques": "Tỷ lệ bệnh nhân có toxicity độ 4 (Grade 4) do Vomiting trong nhóm '$\\ge$65 years' là bao nhiêu?",
+        "ans": "0"
+    },
+    {
+        "image": "data/structural_case/s7.jpg",
+        "ques": "Đối với 'Prostate Cancer Incidence', Block Group 3 có 'Area (km²)' là bao nhiêu?",
+        "ans": "0"
+    },
+    {
+        "image": "data/structural_case/s7.jpg",
+        "ques": "Trong 'Breast Cancer Incidence', 'Cases in Cluster' của Tract 2 là bao nhiêu?",
+        "ans": "6"
+    },
+    {
+        "image": "data/structural_case/s7.jpg",
+        "ques": "Giá trị p-value của 'Block Group 2' trong 'Late Stage Prostate Cancer Incidence' là bao nhiêu?",
+        "ans": "0.0246"
+    },
+    {
+        "image": "data/structural_case/s8.jpg",
+        "ques": "Trong nhóm CYP17, có bao nhiêu gia đình có kiểu gen 1/1 có con gái bị ảnh hưởng (Affected) và tình trạng của mẹ là 'Carrier' (Người mang mầm bệnh)?",
+        "ans": "5"
+    },
+    {
+        "image": "data/structural_case/s8.jpg",
+        "ques": "Đối với kiểu gen 1/2 trong nhóm CYP19 $\\ge$ 10 repeats, tổng số gia đình hạt nhân (Total) dựa trên kiểu gen của cha (Number of nuclear families according to father's genotype) là bao nhiêu?",
+        "ans": "22"
+    },
+    {
+        "image": "data/structural_case/s8.jpg",
+        "ques": "Trong nhóm CYP19 11 repeats, có bao nhiêu con gái không bị ảnh hưởng (Unaffected) mà mẹ có tình trạng 'Non-carrier' (Không mang mầm bệnh) đối với kiểu gen 1/0?",
+        "ans": "16"
+    }
+]
